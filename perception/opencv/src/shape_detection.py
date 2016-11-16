4f9a4991c03cdc23b0a683b31f6f8c87d581c4fc
Alexandre Martins (a.k.a Maral Afris) <maral.afris@gmail.com>
2016-11-16 10:44:45 +0100

    Start to move stuff for distance calculation

--- perception/opencv/src/shape_detection.py
+++ perception/opencv/src/shape_detection.py
@@ -46,7 +46,6 @@ class object_detection:
     def __init__(self, args):
         #Create Rospy Publisher and subscriber
         #self.object_location_pub = rospy.Publisher('/object_location', object_loc, queue_size =1)
-        #original images is huge and creates lot of latency, therefore subscribe to compressed image
 
         #Get image from turtlebot/drones...
         source = Image
@@ -54,16 +53,8 @@ class object_detection:
         if args.mode == 'drone': #Drone
             topic = '/bebop/image_raw/'
             self.modeIsDrone = True
-            #Focal Length of the Asus Prime sensor camera
-            self.focal_leng = 570.34222
-            #This may change during the competetion, need to be calibrated
-            self.square_side_lenth = 0.115 #in mts
         else: #Turtlebot
             topic = '/camera/rgb/image_raw/'
-            #Focal Length of the Asus Prime sensor camera
-            self.focal_leng = 570.34222
-            #This may change during the competition, need to be calibrated
-            self.square_side_lenth = 0.115 #in mts
 
         if args.source is not None and args.source == 'compressed':
             topic += 'compressed/'
@@ -95,7 +86,6 @@ class object_detection:
         #img_cpy = cv_image.copy()
         #Color to HSV and Gray Scale conversion
         hsv = cv2.cvtColor(img_original, cv2.COLOR_BGR2HSV)
-        #gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
         #img = cv_image
         gray = cv2.cvtColor(img_original, cv2.COLOR_BGR2GRAY)
@@ -104,7 +94,6 @@ class object_detection:
             cv2.rectangle(img_for_presentation, (x,y), (x+w, y+h), (255,0,0), 2)
 
         #Thresholds
-
         # worked with one video for TB, but not the other:
         # 0-7 70-220 70-250
         # 190-255 20-255 20-255
@@ -143,78 +132,50 @@ class object_detection:
             print('found Medical Kit!!')
             cv2.rectangle(img_for_presentation, pt, (pt[0]+wRedBoy, pt[1]+hRedBoy), (0,255,255), 2)
 
-        #Find contours(borders) for the shapes in the image
-        #NOTE if you get following error:
-        # contours, hierarchy = cv2.findContours(mask2,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        # ValueError: need more than two values to unpack
-        # change following line to:
-        # contours, hierarchy = cv2.findContours(mask2,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        #marten: Have added "_, " to get it working with new cv2:
-        #_, contours, hierarchy = cv2.findContours(mask2,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        #_, contours, hierarchy = cv2.findContours(greenMask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-        ##Pass through each contour and check if it has required properties to classify into required object
-        #for x in range (len(contours)):
-            #contourarea = cv2.contourArea(contours[x]) #get area of contour
-            #if contourarea > 600: #Discard contours with a small area as this may just be noise
-                ##The below 2 functions help you to approximate the contour to a nearest polygon
-                #arclength = cv2.arcLength(contours[x], True)
-                #approxcontour = cv2.approxPolyDP(contours[x], 0.02 * arclength, True)
-                ##Find the coordinates of the polygon with respect to he camera frame in pixels
-                #rect_cordi = cv2.minAreaRect(contours[x])
-                #obj_x = int(rect_cordi[0][0])
-                #obj_y = int(rect_cordi[0][1])
-
-                ##Check for Square
-                #if len(approxcontour) == 4:
-                    ##print ('Length ', len(approxcontour))
-                    #cv2.drawContours(cv_image,[approxcontour],0,(0,255,255),2)
-                    #approxcontour = approxcontour.reshape((4,2))
-                    #LongestSide = Longest_Length(approxcontour)
-                    #Distance = (self.focal_leng*self.square_side_lenth)/LongestSide #focal length x Actual Border width / size of Border in pixels
-
-                ##Move to next Contour
-                #else :
-                    #continue
-
-                ##Calculate Cordinates wrt to Camera, convert to Map
-                ##Coordinates and publish message for storing
-                ##319.5, 239.5 = image centre
-                #obj_cam_x = ((obj_x - 319.5)*Distance)/self.focal_leng
-                #obj_cam_y = ((obj_y - 239.5)*Distance)/self.focal_leng
-
-                ##convert the x,y in camera frame to a geometric stamped point
-                #P = PointStamped()
-                #P.header.stamp = rospy.Time.now() - rospy.Time(23)
-                ##print ('time: ', data.header.stamp)
-                #P.header.frame_id = 'camera_rgb_optical_frame'
-                #P.point.x = obj_cam_x
-                #P.point.y = obj_cam_y
-                #P.point.z = Distance
-
-                #Transform Point into map coordinates
-                #trans_pt = self.tl.transformPoint('/map', P)
-
-                #fill in the publisher object to publish
-                #marten obj_info_pub = object_loc()
-                #marten obj_info_pub.ID = 27 #ID need to be changed
-                #obj_info_pub.point.x = trans_pt.point.x
-                #obj_info_pub.point.y = trans_pt.point.y
-                #obj_info_pub.point.z = trans_pt.point.z
-
-                #publish the message
-                #marten self.object_location_pub.publish(obj_info_pub)
-
         #Display the captured image
-        #cv2.imshow("Image",cv_image)
-        #cv2.imshow("HSV", hsv)
-        #cv2.imshow("res",res)
-        #cv2.imshow("Green screen",green_cv_image)
-        #cv2.imshow("Blue screen",blue_cv_image)
         cv2.imshow("img",img_for_presentation)
         cv2.imshow("Red screen",red_cv_image)
         cv2.waitKey(1)
 
+
+#Calculate coordinates according to picture size and stuff
+def calc_coord(x, y, w, h, source):
+"""
+    if source == 'turtlebot':
+        focal_leng = 570.34222
+        square_side_lenth = 0.115 #in mts
+    else:
+        focal_leng = 570.34222
+        square_side_lenth = 0.115 #in mts
+
+    #Calculate Cordinates wrt to Camera, convert to Map
+    #Coordinates and publish message for storing
+    #319.5, 239.5 = image centre
+    obj_cam_x = ((obj_x - 319.5)*Distance)/self.focal_leng
+    obj_cam_y = ((obj_y - 239.5)*Distance)/self.focal_leng
+
+    #convert the x,y in camera frame to a geometric stamped point
+    P = PointStamped()
+    P.header.stamp = rospy.Time.now() - rospy.Time(23)
+    #print ('time: ', data.header.stamp)
+    P.header.frame_id = 'camera_rgb_optical_frame'
+    P.point.x = obj_cam_x
+    P.point.y = obj_cam_y
+    P.point.z = Distance
+
+    #Transform Point into map coordinates
+    trans_pt = self.tl.transformPoint('/map', P)
+
+    #fill in the publisher object to publish
+    obj_info_pub = object_loc()
+    obj_info_pub.ID = 27 #ID need to be changed
+    obj_info_pub.point.x = trans_pt.point.x
+    obj_info_pub.point.y = trans_pt.point.y
+    obj_info_pub.point.z = trans_pt.point.z
+
+    #publish the message
+    self.object_location_pub.publish(obj_info_pub)
+"""
 
 #Check validity of the mode argument provided
 def check_mode(v):
