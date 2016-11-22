3d3ab6a18925832b47c952083c5224982c4047d7
Alexandre Martins (a.k.a Maral Afris) <maral.afris@gmail.com>
2016-11-22 10:57:26 +0100

    Add shell script to launch the turtlebot

---
+++ turtlebot_launch.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#Force killing children when exiting
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+#Minimal launch
+echo "ROS minimal launch"
+roslaunch turtlebot_bringup minimal.launch --screen > roslaunch.log 2>&1 &
+sleep 30
+
+#Lidar
+echo "ROS lidar launch"
+roslaunch rplidar_ros rplidar.launch > lidar_launch.log 2>&1 &
+sleep 10
+rosrun rplidar_ros rplidarNodeClient > lidar_ros.log 2>&1 &
+sleep 10
+
+#Static transform
+echo "ROS static transform launch"
+rosrun tf static_transform_publisher 0.0 0.0 0.0 0.0 0.0 0.0 base_link laser 100 > tf.log 2>&1 &
+sleep 10
+
+#Navigation
+echo "ROS navigation launch"
+roslaunch turtlebot_navigation amcl_demo.launch > navigation.log 2>&1 &
+sleep 10
+
+echo "ROS: All launched !"
+echo "Press CTRL+C to stop everything"
+while true; do
+    : #Nothing
+done
