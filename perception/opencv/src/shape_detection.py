8d2834f366102311472534a5e573ba9e06533d9c
Alexandre Martins (a.k.a Maral Afris) <maral.afris@gmail.com>
2016-11-23 09:12:20 +0100

    Do coordinate filtering

--- perception/opencv/src/shape_detection.py
+++ perception/opencv/src/shape_detection.py
@@ -21,6 +21,13 @@ import tf
 
 import argparse
 
+class obj_map_item:
+    def __init__(self, obj_type_id=None, map_x=0, map_y=0, map_z=0):
+        self.t = obj_type_id
+        self.x = map_x
+        self.y = map_y
+        self.z = map_z
+
 #This is the main class for object detection, it has some initializations about nodes
 #Call back functions etc
 class object_detection:
@@ -53,6 +60,8 @@ class object_detection:
         self.grayGreenBoy = cv2.imread('BlackPerson.png',0)
 
         self.body_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+        self.known_obj_map_list = [] #List containing detected objects
+        self.obj_map_margin = 10 #Margin to consider for avoiding repetitive objects
         self.jumpOver = 1
 
     #Callback function for subscribed image
@@ -75,7 +84,7 @@ class object_detection:
             gray = cv2.cvtColor(img_original, cv2.COLOR_BGR2GRAY)
             bodies = self.body_cascade.detectMultiScale(gray,1.3,5)
             for (x,y,w,h) in bodies:
-                print('Found a person!')
+                #print('Found a person!')
                 self.calc_coord(w, h, w, h, 'person')
                 cv2.rectangle(img_for_presentation, (x,y), (x+w, y+h), (255,0,0), 2)
 
@@ -118,11 +127,11 @@ class object_detection:
             locMedBoxSmall = np.where(medBoxSmallMatchingResult >=thresholdMedBoxSmall)
             locMedBoxLarge = np.where(medBoxLargeMatchingResult >=thresholdMedBoxLarge)
             for pt in zip (*locMedBoxSmall[::-1]):
-                print('found Medical Kit far away!!')
+                #print('found Medical Kit far away!!')
                 self.calc_coord(pt[0], pt[1], wMedBoxSmall, hMedBoxSmall, 'medkit_far')
                 cv2.rectangle(img_for_presentation, pt, (pt[0]+wMedBoxSmall, pt[1]+hMedBoxSmall), (0,255,255), 2)
             for pt in zip (*locMedBoxLarge[::-1]):
-                print('found Medical Kit near!!')
+                #print('found Medical Kit near!!')
                 self.calc_coord(pt[0], pt[1], wMedBoxLarge, hMedBoxLarge, 'medkit_near')
                 cv2.rectangle(img_for_presentation, pt, (pt[0]+wMedBoxLarge, pt[1]+hMedBoxSmall), (50,200,200), 2)
             #Display the captured image
@@ -136,7 +145,7 @@ class object_detection:
 
     #Calculate coordinates according to picture size and stuff
     def calc_coord(self, x, y, w, h, obj):
-        print('Received: x:%d y:%d w:%d h:%d obj:%s' %(x, y, w, h, obj))
+        #print('Received: x:%d y:%d w:%d h:%d obj:%s' %(x, y, w, h, obj))
         #Image center
         ctr_x = 239.5
         ctr_y = 319.5
@@ -152,17 +161,17 @@ class object_detection:
             obj_orig_w = 17.5 #cm
             obj_orig_h = 17.5 #cm
             #obj_orig_d = 1 #cm
-            obj_id = 1
+            obj_type_id = 1
         if obj == 'medkit_far':
             obj_orig_w = 17.5 #cm
             obj_orig_h = 17.5 #cm
             #obj_orig_d = 1 #cm
-            obj_id = 1
+            obj_type_id = 1
         elif obj == 'person':
             obj_orig_w = 10 #cm
             obj_orig_h = 26 #cm
             #obj_orig_d = 1 #cm
-            obj_id = 2
+            obj_type_id = 2
         else:
             return None
 
@@ -176,8 +185,8 @@ class object_detection:
         obj_mid_y = y + h/2
         obj_cam_x = ((obj_mid_x - ctr_x)*dist) / focal_leng
         obj_cam_y = ((obj_mid_y - ctr_y)*dist) / focal_leng
-        print('Drone:%d dist:%dcm (x:%d,y:%d), cam:x:%d,y:%d' %(self.modeIsDrone,
-              dist, obj_dist_x, obj_dist_y, obj_cam_x, obj_cam_y))
+        #print('Drone:%d dist:%dcm (x:%d,y:%d), cam:x:%d,y:%d' %(self.modeIsDrone,
+        #      dist, obj_dist_x, obj_dist_y, obj_cam_x, obj_cam_y))
 
         #convert the x,y in camera frame to a geometric stamped point
         P = PointStamped()
@@ -189,16 +198,61 @@ class object_detection:
 
         #Transform Point into map coordinates
         trans_pt = self.tl.transformPoint('/map', P)
+        if not self.obj_exists(trans_pt, obj_type_id):
+            self.publish_obj(trans_pt, obj_type_id)
 
+    def obj_exists(self, map_pt, obj_type):
+        '''
+        Verifies if the object at map_pt is already in the map.
+        If so return True, otherwise return False and add to the list of known obj
+        '''
+        margin = 10
+        margin_big = 20
+        obj_found = False
+
+        #Get new map point
+        nx = int(map_pt.point.x)
+        ny = int(map_pt.point.y)
+        nz = int(map_pt.point.z)
+
+        for o in self.known_obj_map_list:
+            if o.t != obj_type:
+                #Object type differs
+                continue
+
+            #Check if coordinates are the same, if so we found it !
+            ox = range(int(o.x - margin), int(o.x + margin))
+            oy = range(int(o.y - margin), int(o.y + margin))
+            oz = range(int(o.z - margin_big), int(o.z + margin_big))
+            if (nx in ox) and (ny in oy) and (nz in oz):
+                obj_found = True
+                break
+
+        #If object was not found, add it to the list :)
+        if not obj_found:
+            new_obj = obj_map_item(obj_type_id=obj_type, map_x=nx,
+                                   map_y=ny, map_z=nz)
+            self.known_obj_map_list.append(new_obj)
+            print('Added t:%d x:%d y:%d z:%d to list (now %d elements)' %(new_obj.t,
+                  new_obj.x, new_obj.y, new_obj.z, len(self.known_obj_map_list)))
+
+        return obj_found
+
+    def publish_obj(self, map_pt, obj_type):
+        '''
+        Publishes the object coordinates according to the map
+        '''
         #Fill in the publisher object to publish
         obj_info_pub = object_loc()
-        obj_info_pub.ID = obj_id
-        obj_info_pub.point.x = trans_pt.point.x
-        obj_info_pub.point.y = trans_pt.point.y
-        obj_info_pub.point.z = trans_pt.point.z
+        obj_info_pub.ID = obj_type
+        obj_info_pub.point.x = map_pt.point.x
+        obj_info_pub.point.y = map_pt.point.y
+        obj_info_pub.point.z = map_pt.point.z
 
         #Publish the message
         self.object_location_pub.publish(obj_info_pub)
+        print('Published obj ID:%d at x:%d y:%d z:%d' %(obj_type, map_pt.point.x,
+                                                        map_pt.point.y, map_pt.point.z))
 
 
 #Check validity of the mode argument provided
